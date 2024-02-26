9e72ba5c5d9768be5d15a454ecdd7a59e632da85
Benjamin <benjamin_gaertner@gmx.net>
2024-02-26 17:38:52 +0100

    Added basic project structure

---
+++ src/client/java/sh4d0w/theonesword/TheOneSwordClient.java
@@ -0,0 +1,10 @@
+package sh4d0w.theonesword;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheOneSwordClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
