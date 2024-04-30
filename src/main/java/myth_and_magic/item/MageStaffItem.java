71ab0683bc82b6199bc74bf9c21272471f828706
Benjamin <benjamin_gaertner@gmx.net>
2024-04-30 18:06:40 +0200

    Changed mage staff from charge to cooldown

--- src/main/java/myth_and_magic/item/MageStaffItem.java
+++ src/main/java/myth_and_magic/item/MageStaffItem.java
@@ -9,7 +9,6 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 
 import java.util.function.Predicate;
@@ -29,56 +28,28 @@ public class MageStaffItem extends RangedWeaponItem {
         return 15;
     }
 
-    public static float getPullProgress(int useTicks) {
-        float f = (float) useTicks / 20.0F;
-        f = (f * f + f * 2.0F) / 3.0F;
-        if (f > 1.0F) {
-            f = 1.0F;
-        }
-        return f;
-    }
-
-    public int getMaxUseTime(ItemStack stack) {
-        return 60000;
-    }
-
-    public UseAction getUseAction(ItemStack stack) {
-        return UseAction.BOW;
-    }
-
-    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
-        ItemStack itemStack = user.getProjectileType(stack);
-        if (!itemStack.isEmpty()) {
-            int i = this.getMaxUseTime(stack) - remainingUseTicks;
-            float f = getPullProgress(i);
-            if (f == 1f) {
-                if (!world.isClient()) {
-                    RuneProjectileEntity runeProjectileEntity = new RuneProjectileEntity(user, world, itemStack, (PlayerEntity) user);
-                    runeProjectileEntity.setItem(itemStack);
-                    runeProjectileEntity.setVelocity(user, user.getPitch(), user.getYaw(), user.getRoll(), 3f, 0f);
-                    world.spawnEntity(runeProjectileEntity);
-                }
-                world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ITEM_FIRECHARGE_USE,
-                        SoundCategory.NEUTRAL, 1f, 1f);
-                itemStack.decrement(1);
-                if (itemStack.isEmpty()) {
-                    ((PlayerEntity) user).getInventory().removeOne(itemStack);
-                }
-                stack.damage(1, user, (p) -> {
-                    p.sendToolBreakStatus(user.getActiveHand());
-                });
-            }
-        }
-    }
-
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
         ItemStack projectile = user.getProjectileType(itemStack);
-        if (projectile.isEmpty() || projectile.isOf(Items.ARROW)) {
+        if (projectile.isEmpty() || !projectile.isIn(MythAndMagicItemTagProvider.RUNE_PROJECTILES)) {
             return TypedActionResult.fail(itemStack);
         } else {
             user.setCurrentHand(hand);
-            return TypedActionResult.consume(itemStack);
+            if (!world.isClient()) {
+                RuneProjectileEntity runeProjectileEntity = new RuneProjectileEntity(user, world, projectile, user);
+                runeProjectileEntity.setItem(projectile);
+                runeProjectileEntity.setVelocity(user, user.getPitch(), user.getYaw(), user.getRoll(), 3f, 0f);
+                world.spawnEntity(runeProjectileEntity);
+            }
+            world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ITEM_FIRECHARGE_USE,
+                    SoundCategory.NEUTRAL, 1f, 1f);
+            projectile.decrement(1);
+            if (projectile.isEmpty()) {
+                user.getInventory().removeOne(projectile);
+            }
+            itemStack.damage(1, user, (p) -> p.sendToolBreakStatus(hand));
+            user.getItemCooldownManager().set(this, 40);
+            return TypedActionResult.success(itemStack);
         }
     }
 }
